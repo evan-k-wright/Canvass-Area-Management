5a653fc5ba26d6713bf63556467ea785842e2723
Evan Wright <rideburtonsnow3@gmail.com>
2018-12-05 11:09:02 -0700

    finished code

--- testing/areaManagement.js
+++ testing/areaManagement.js
@@ -1,44 +1,44 @@
-// const data = require('../sources/data')
+const data = require('../sources/data')
 
-// module.exports = {
-//     after: browser => {
-//         browser
-//     },
+module.exports = {
+    after: browser => {
+        browser.end()
+    },
 
-//     'Area Management': (browser) => {
-//         let areaManagement = browser.page.areaManagement()
-//         areaManagement.navigate()
-//             //login
-//                 .waitForElementVisible('@companyURL', 5000)
-//                 .waitForElementVisible('@username', 5000)
-//                 .waitForElementVisible('@password', 5000)
-//                 .setValue('@companyURL', data.company)
-//                 .setValue('@username', data.username)
-//                 .setValue('@password', data.password)
-//                 .waitForElementVisible('@loginButton', 5000)
-//                 .click('@loginButton')
-//                 .waitForElementVisible('@salesHub', 5000)
-//                 .click('@salesHub')
-//                 .waitForElementVisible('@canvassApp', 5000)
-//                 .click('@canvassApp')
-//                 .waitForElementVisible('@canvass', 5000) 
+    'Area Management': (browser) => {
+        let areaManagement = browser.page.areaManagement()
+        areaManagement.navigate()
+            //login
+                .waitForElementVisible('@companyURL', 5000)
+                .waitForElementVisible('@username', 5000)
+                .waitForElementVisible('@password', 5000)
+                .setValue('@companyURL', data.company)
+                .setValue('@username', data.username)
+                .setValue('@password', data.password)
+                .waitForElementVisible('@loginButton', 5000)
+                .click('@loginButton')
+                .waitForElementVisible('@salesHub', 5000)
+                .click('@salesHub')
+                .waitForElementVisible('@canvassApp', 5000)
+                .click('@canvassApp')
+                .waitForElementVisible('@canvass', 5000) 
 
-//             //adding area by zip
-//                 // .waitForElementVisible('@canvassArea', 5000)
-//                 // .click('@canvassArea')
-//                 // .waitForElementVisible('@areas', 5000)
-//                 // .click('@provo')
-//                 // .waitForElementVisible('@newAreaZip', 5000)
-//                 // .click('@newAreaZip')
-//                 // .api.keys(["8", "4", "1", "0", "4"])
+            //adding area by zip
+                // .waitForElementVisible('@canvassArea', 5000)
+                // .click('@canvassArea')
+                // .waitForElementVisible('@areas', 5000)
+                // .click('@provo')
+                // .waitForElementVisible('@newAreaZip', 5000)
+                // .click('@newAreaZip')
+                // .api.keys(["8", "4", "1", "0", "4"])
 
-//             //select area
-//                 .waitForElementVisible('@canvassArea', 5000)
-//                 .click('@canvassArea')
-//                 .waitForElementVisible('@areas', 5000)
-//                 .api.pause(5000)
-//                 areaManagement.click('@provo')
-//                 .waitForElementVisible('@testArea2', 10000)
-//                 .click('@testArea2')
-//     }
-// }
+            //select area
+                .waitForElementVisible('@canvassArea', 5000)
+                .click('@canvassArea')
+                .waitForElementVisible('@areas', 5000)
+                .api.pause(5000)
+                areaManagement.click('@provo')
+                .waitForElementVisible('@testArea2', 10000)
+                .click('@testArea2')
+    }
+}
