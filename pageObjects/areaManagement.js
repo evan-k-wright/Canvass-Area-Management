37eb5870e5cc487e781305dd360ad7d8ac6f7faf
Evan Wright <rideburtonsnow3@gmail.com>
2019-01-15 15:43:34 -0700

    fixed code

--- pageObjects/areaManagement.js
+++ pageObjects/areaManagement.js
@@ -1,41 +1,41 @@
-// module.exports = {
-//     url: 'http://admin.salesos.info/users/login',
-//     elements: {
-//         //login
-//             canvassApp: 'a[class="enabled app-canvass"]',
-//             canvass: 'div[class="current-app"]',
-//             companyURL: 'input[placeHolder="Company URL"]',
-//             loginButton: 'button[type="submit"]', 
-//             password: 'input[placeHolder="Password"]',
-//             salesHub: 'div[class="current-app"]',
-//             username: 'input[placeHolder="Username"]',
+module.exports = {
+    url: 'http://admin.salesos.info/users/login',
+    elements: {
+        //login
+            canvassApp: 'a[class="enabled app-canvass"]',
+            canvass: 'div[class="current-app"]',
+            companyURL: 'input[placeHolder="Company URL"]',
+            loginButton: 'button[type="submit"]', 
+            password: 'input[placeHolder="Password"]',
+            salesHub: 'div[class="current-app"]',
+            username: 'input[placeHolder="Username"]',
 
-//         //select area
-//             areaTitle: 'h1[id="areaName"]', 
-//             canvassArea: 'a[href="/canvass/area"]',
-//             "000Testing": 'option[value="127"]',
-//             evansArea: 'tr[data-name="evan\'s area"]',
+        //select area
+            areaTitle: 'h1[id="areaName"]', 
+            canvassArea: 'a[href="/canvass/area"]',
+            "000Testing": 'option[value="127"]',
+            evansArea: 'tr[data-name="evan\'s area"]',
 
-//             //xpath
-//                 areas: {
-//                     selector: '(//h1)[1]',
-//                     locateStrategy: 'xpath'
-//                 },
+            //xpath
+                areas: {
+                    selector: '(//h1)[1]',
+                    locateStrategy: 'xpath'
+                },
 
-//                 // testArea2: {
-//                 //     selector: '//span[.="Test Area 2"]',
-//                 //     locateStrategy: 'xpath'
-//                 // },
+                // testArea2: {
+                //     selector: '//span[.="Test Area 2"]',
+                //     locateStrategy: 'xpath'
+                // },
 
-//         //adding area by zip
-//             canvassArea: 'a[href="/canvass/area"]',
-//             newAreaZip: 'a[id="drawZipArea"]',
-//             "000Testing": 'option[value="127"]',
+        //adding area by zip
+            canvassArea: 'a[href="/canvass/area"]',
+            newAreaZip: 'a[id="drawZipArea"]',
+            "000Testing": 'option[value="127"]',
 
-//             //xpath
-//                 areas: {
-//                     selector: '(//h1)[1]',
-//                     locateStrategy: 'xpath'
-//                 },
-//     }
-// }
+            //xpath
+                areas: {
+                    selector: '(//h1)[1]',
+                    locateStrategy: 'xpath'
+                },
+    }
+}
--- testing/areaManagement.js
+++ testing/areaManagement.js
@@ -1,45 +1,45 @@
-const data = require('../sources/data')
+// const data = require('../sources/data')
 
-module.exports = {
-    after: browser => {
-        browser.end()
-    },
+// module.exports = {
+//     after: browser => {
+//         browser.end()
+//     },
 
-    'Area Management': (browser) => {
-        let areaManagement = browser.page.areaManagement()
-        areaManagement.navigate()
-            //login
-                .waitForElementVisible('@companyURL', 5000)
-                .waitForElementVisible('@username', 5000)
-                .waitForElementVisible('@password', 5000)
-                .setValue('@companyURL', data.company)
-                .setValue('@username', data.username)
-                .setValue('@password', data.password)
-                .waitForElementVisible('@loginButton', 5000)
-                .click('@loginButton')
-                .waitForElementVisible('@salesHub', 5000)
-                .click('@salesHub')
-                .waitForElementVisible('@canvassApp', 5000)
-                .click('@canvassApp')
-                .waitForElementVisible('@canvass', 5000) 
+//     'Area Management': (browser) => {
+//         let areaManagement = browser.page.areaManagement()
+//         areaManagement.navigate()
+//             //login
+//                 .waitForElementVisible('@companyURL', 5000)
+//                 .waitForElementVisible('@username', 5000)
+//                 .waitForElementVisible('@password', 5000)
+//                 .setValue('@companyURL', data.company)
+//                 .setValue('@username', data.username)
+//                 .setValue('@password', data.password)
+//                 .waitForElementVisible('@loginButton', 5000)
+//                 .click('@loginButton')
+//                 .waitForElementVisible('@salesHub', 5000)
+//                 .click('@salesHub')
+//                 .waitForElementVisible('@canvassApp', 5000)
+//                 .click('@canvassApp')
+//                 .waitForElementVisible('@canvass', 5000) 
 
-            //adding area by zip
-                // .waitForElementVisible('@canvassArea', 5000)
-                // .click('@canvassArea')
-                // .waitForElementVisible('@areas', 5000)
-                // .click('@p000Testing')
-                // .waitForElementVisible('@newAreaZip', 5000)
-                // .click('@newAreaZip')
-                // .api.keys(["8", "4", "1", "0", "4"])
+//             //adding area by zip
+//                 // .waitForElementVisible('@canvassArea', 5000)
+//                 // .click('@canvassArea')
+//                 // .waitForElementVisible('@areas', 5000)
+//                 // .click('@p000Testing')
+//                 // .waitForElementVisible('@newAreaZip', 5000)
+//                 // .click('@newAreaZip')
+//                 // .api.keys(["8", "4", "1", "0", "4"])
 
-            //select area
-                .waitForElementVisible('@canvassArea', 5000)
-                .click('@canvassArea')
-                .waitForElementVisible('@areas', 5000)
+//             //select area
+//                 .waitForElementVisible('@canvassArea', 5000)
+//                 .click('@canvassArea')
+//                 .waitForElementVisible('@areas', 5000)
                 
-                .click('@000Testing')
-                .waitForElementVisible('@evansArea', 10000)
-                .click('@evansArea')
-                .waitForElementPresent('@areaTitle', 5000)
-    }
-}
+//                 .click('@000Testing')
+//                 .waitForElementVisible('@evansArea', 10000)
+//                 .click('@evansArea')
+//                 .waitForElementPresent('@areaTitle', 5000)
+//     }
+// }
