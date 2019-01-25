83c090aec434f3f2f1ff663bf14229e873b467c1
Evan Wright <rideburtonsnow3@gmail.com>
2019-01-25 14:22:34 -0700

    added to selectors file. changed test code

--- sources/selectors.js
+++ sources/selectors.js
@@ -1,3 +1,17 @@
 module.exports = {
-    remove: 'a[class="remove-rep"]',
+    //login
+        canvassApp: 'a[class="enabled app-canvass"]',
+        canvass: 'div[class="current-app"]',
+        companyURL: 'input[placeHolder="Company URL"]',
+        loginButton: 'button[type="submit"]', 
+        password: 'input[placeHolder="Password"]',
+        salesHub: 'div[class="current-app"]',
+        username: 'input[placeHolder="Username"]',
+
+    //assign rep
+        remove: 'a[class="remove-rep"]',
+
+    //add area zip
+        areaMang: 'a[href="/canvass/area"]',
+        drawAreaZip: 'a[id="drawZipArea"]',
 }
