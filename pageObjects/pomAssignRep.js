2d7d1fd2388bb3d71f5dc1d5f9f5f9b63a1892f1
Evan Wright <rideburtonsnow3@gmail.com>
2019-01-25 11:08:02 -0700

    updated selector

--- pageObjects/pomAssignRep.js
+++ pageObjects/pomAssignRep.js
@@ -27,7 +27,7 @@ module.exports = {
                 // },
 
                 removeRep: {
-                    selector: '//a[class="remove-rep"]',
+                    selector: '//a[@class="remove-rep"]',
                     locateStrategy: 'xpath'
                 },
 
