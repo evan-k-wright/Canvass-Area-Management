21ed8644f5da40406c56a87a33b90db8886f0b89
Evan Wright <rideburtonsnow3@gmail.com>
2019-03-15 09:32:45 -0600

    updated code

--- testing/areaManagement.js
+++ testing/areaManagement.js
@@ -1,36 +1,36 @@
-const data = require('../sources/data')
+// const data = require('../sources/data')
 
-module.exports = {
-    after: browser => {
-        browser.end()
-    },
+// module.exports = {
+//     after: browser => {
+//         browser.end()
+//     },
 
-    'Area Management': (browser) => {
-        let pomAreaManagement = browser.page.pomAreaManagement()
-        pomAreaManagement.navigate()
-            //login
-                .waitForElementVisible('@companyURL', 5000)
-                .waitForElementVisible('@username', 5000)
-                .waitForElementVisible('@password', 5000)
-                .setValue('@companyURL', data.company)
-                .setValue('@username', data.username)
-                .setValue('@password', data.password)
-                .waitForElementVisible('@loginButton', 5000)
-                .click('@loginButton')
-                .waitForElementVisible('@salesHub', 5000)
-                .click('@salesHub')
-                .waitForElementVisible('@canvassApp', 5000)
-                .click('@canvassApp')
-                .waitForElementVisible('@canvass', 5000) 
+//     'Area Management': (browser) => {
+//         let pomAreaManagement = browser.page.pomAreaManagement()
+//         pomAreaManagement.navigate()
+//             //login
+//                 .waitForElementVisible('@companyURL', 5000)
+//                 .waitForElementVisible('@username', 5000)
+//                 .waitForElementVisible('@password', 5000)
+//                 .setValue('@companyURL', data.company)
+//                 .setValue('@username', data.username)
+//                 .setValue('@password', data.password)
+//                 .waitForElementVisible('@loginButton', 5000)
+//                 .click('@loginButton')
+//                 .waitForElementVisible('@salesHub', 5000)
+//                 .click('@salesHub')
+//                 .waitForElementVisible('@canvassApp', 5000)
+//                 .click('@canvassApp')
+//                 .waitForElementVisible('@canvass', 5000) 
 
-            //select area
-                .waitForElementVisible('@canvassArea', 5000)
-                .click('@canvassArea')
-                .waitForElementVisible('@areas', 5000)
+//             //select area
+//                 .waitForElementVisible('@canvassArea', 5000)
+//                 .click('@canvassArea')
+//                 .waitForElementVisible('@areas', 5000)
                 
-                .click('@000Testing')
-                .waitForElementVisible('@evansArea', 10000)
-                .click('@evansArea')
-                .waitForElementPresent('@areaTitle', 5000)
-    }
-}
+//                 .click('@000Testing')
+//                 .waitForElementVisible('@evansArea', 10000)
+//                 .click('@evansArea')
+//                 .waitForElementPresent('@areaTitle', 5000)
+//     }
+// }
