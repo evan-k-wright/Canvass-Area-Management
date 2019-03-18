b485509519a4542bc8e4c5bd10bc99c2cff99ad6
Evan Wright <rideburtonsnow3@gmail.com>
2019-03-18 10:20:36 -0600

    commeneted and uncommented code for testing

--- testing/configurePins.js
+++ testing/configurePins.js
@@ -1,45 +1,45 @@
-const data = require('../sources/data')
+// const data = require('../sources/data')
 
-module.exports = {
-    after: browser => {
-        //browser.end()
-    },
+// module.exports = {
+//     after: browser => {
+//         //browser.end()
+//     },
 
-    'Configure Pins': browser => {
-        let pomConfigurePins = browser.page.pomConfigurePins()
-        pomConfigurePins.navigate()
-            //login
-                .waitForElementVisible('@companyURL', 5000)
-                .waitForElementVisible('@username', 5000)
-                .waitForElementVisible('@password', 5000)
-                .setValue('@companyURL', data.company)
-                .setValue('@username', data.username)
-                .setValue('@password', data.password)
-                .waitForElementVisible('@loginButton', 5000)
-                .click('@loginButton')
-                .waitForElementVisible('@salesHub', 5000)
-                .click('@salesHub')
-                .waitForElementVisible('@canvassApp', 5000)
-                .click('@canvassApp')
-                .waitForElementVisible('@canvass', 5000) 
+//     'Configure Pins': browser => {
+//         let pomConfigurePins = browser.page.pomConfigurePins()
+//         pomConfigurePins.navigate()
+//             //login
+//                 .waitForElementVisible('@companyURL', 5000)
+//                 .waitForElementVisible('@username', 5000)
+//                 .waitForElementVisible('@password', 5000)
+//                 .setValue('@companyURL', data.company)
+//                 .setValue('@username', data.username)
+//                 .setValue('@password', data.password)
+//                 .waitForElementVisible('@loginButton', 5000)
+//                 .click('@loginButton')
+//                 .waitForElementVisible('@salesHub', 5000)
+//                 .click('@salesHub')
+//                 .waitForElementVisible('@canvassApp', 5000)
+//                 .click('@canvassApp')
+//                 .waitForElementVisible('@canvass', 5000) 
 
-            //pins
-                .waitForElementVisible('@canvassArea', 5000)
-                .click('@canvassArea')
-                .waitForElementVisible('@pinsList', 5000)
-                .click('@pinsList')
-                .waitForElementVisible('@sale', 5000)
-                .waitForElementVisible('@followUp', 5000)
-                .click('@sale')
-                .click('@followUp')
-                .waitForElementVisible('@updatePins', 5000)
-                .click('@updatePins')
-                .waitForElementVisible('@salePin', 5000)
-                .waitForElementVisible('@followUpPin', 5000)
-                .click('@sale')
-                .click('@followUp')
-                .click('@updatePins')
-                .api.pause(500)
-                .acceptAlert()
-    }
-}
+//             //pins
+//                 .waitForElementVisible('@canvassArea', 5000)
+//                 .click('@canvassArea')
+//                 .waitForElementVisible('@pinsList', 5000)
+//                 .click('@pinsList')
+//                 .waitForElementVisible('@sale', 5000)
+//                 .waitForElementVisible('@followUp', 5000)
+//                 .click('@sale')
+//                 .click('@followUp')
+//                 .waitForElementVisible('@updatePins', 5000)
+//                 .click('@updatePins')
+//                 .waitForElementVisible('@salePin', 5000)
+//                 .waitForElementVisible('@followUpPin', 5000)
+//                 .click('@sale')
+//                 .click('@followUp')
+//                 .click('@updatePins')
+//                 .api.pause(500)
+//                 .acceptAlert()
+//     }
+// }
--- testing/editArea.js
+++ testing/editArea.js
@@ -1,41 +1,41 @@
-// const data = require('../sources/data')
+const data = require('../sources/data')
 
-// module.exports = {
-//     after: browser => {
-//         browser
-//     },
+module.exports = {
+    after: browser => {
+        browser
+    },
 
-//     'Edit Area': browser => {
-//         let pomEditArea = browser.page.pomEditArea()
-//         pomEditArea.navigate()
-//             //login
-//                 .waitForElementVisible('@companyURL', 5000)
-//                 .waitForElementVisible('@username', 5000)
-//                 .waitForElementVisible('@password', 5000)
-//                 .setValue('@companyURL', data.company)
-//                 .setValue('@username', data.username)
-//                 .setValue('@password', data.password)
-//                 .waitForElementVisible('@loginButton', 5000)
-//                 .click('@loginButton')
-//                 .waitForElementVisible('@salesHub', 5000)
-//                 .click('@salesHub')
-//                 .waitForElementVisible('@canvassApp', 5000)
-//                 .click('@canvassApp')
-//                 .waitForElementVisible('@canvass', 5000) 
+    'Edit Area': browser => {
+        let pomEditArea = browser.page.pomEditArea()
+        pomEditArea.navigate()
+            //login
+                .waitForElementVisible('@companyURL', 5000)
+                .waitForElementVisible('@username', 5000)
+                .waitForElementVisible('@password', 5000)
+                .setValue('@companyURL', data.company)
+                .setValue('@username', data.username)
+                .setValue('@password', data.password)
+                .waitForElementVisible('@loginButton', 5000)
+                .click('@loginButton')
+                .waitForElementVisible('@salesHub', 5000)
+                .click('@salesHub')
+                .waitForElementVisible('@canvassApp', 5000)
+                .click('@canvassApp')
+                .waitForElementVisible('@canvass', 5000) 
 
-//             //edit area
-//                 .waitForElementVisible('@areaMang', 5000)
-//                 .click('@areaMang')
-//                 .waitForElementVisible('@area', 5000)
-//                 .click('@area')
-//                 .waitForElementPresent('@edit', 5000)
-//                 .api.pause(500)
-//                 pomEditArea.click('@edit')
-//                 .api.setAlertText("Test Area 4")
-//                 .acceptAlert()
-//                 pomEditArea.waitForElementVisible('@back', 5000)
-//                 .click('@back')
-//                 .api.refresh()
-//                 pomEditArea.waitForElementVisible('@newAreaName', 5000)
-//     }
-// }
+            //edit area
+                .waitForElementVisible('@areaMang', 5000)
+                .click('@areaMang')
+                .waitForElementVisible('@area', 5000)
+                .click('@area')
+                .waitForElementPresent('@edit', 5000)
+                .api.pause(500)
+                pomEditArea.click('@edit')
+                .api.setAlertText("Test Area 4")
+                .acceptAlert()
+                pomEditArea.waitForElementVisible('@back', 5000)
+                .click('@back')
+                .api.refresh()
+                pomEditArea.waitForElementVisible('@newAreaName', 5000)
+    }
+}
